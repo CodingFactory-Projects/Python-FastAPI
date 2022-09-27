a79c3fc7ddeec9f9ff2ee4c9ae8470d98de4e0ae
Elim Florvil <elim.florvil@icloud.com>
2022-09-27 16:35:21 +0200

    Update main.py
    création avec post et condition

--- main.py
+++ main.py
@@ -7,33 +7,64 @@ app = FastAPI()
 
 class Item(BaseModel):
     name: str
+    description: str | None = None
     price: float
-    is_offer: Union[bool, None] = None
+    tax: float | None = None
 
-## Routes ROOT
+
+"""Routes ROOT"""
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-## Update all in item by item_id
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
-## Routes display name,age and short description
-@app.get("/peoples/{name}/{age}")
+"""Routes USERS"""
+
+
+"""Routes display name,age and short description"""
+@app.get("/users/{name}/{age}")
 def get_name_with_age(name: str, age: int):
     name_with_age = name + " is this old " + str(age)+"years old."
     return {"name": name, "age": age, "description": name_with_age}
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+"""Routes display item with multiple query parameters"""
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
-## Routes display item with query parameters (skip,limit)
+"""Routes display users with 3 query parameters"""
+@app.get("/users/{item_id}")
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+
+"""Routes ITEMS"""
+
+""" CREATE ITEMS with condition   """
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+"""Routes display item with query parameters (skip,limit)"""
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
 
-## Read item by item.id with condition for query paramaters
+"""Read item by item.id with condition for query paramaters"""
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
     item = {"item_id": item_id}
@@ -45,29 +76,17 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
         )
     return item
 
-## Routes display item with multiple query parameters
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+"""Update all in item by item_id"""
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_price": item.price, "item_id": item_id}
 
-## Routes display produit with required query parameters
+"""Routes PRODUITS"""
+
+"""Routes display produit with required query parameters"""
 @app.get("/produits/{item_id}")
 async def read_user_item(item_id: str, requis: str):
     item = {"item_id": item_id, "requis": requis}
     return item
-## Routes display shoes with 3 query parameters
-@app.get("/shoes/{item_id}")
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: int | None = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+
+
