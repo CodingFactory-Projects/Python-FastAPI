8b3b03580e4818b7c27a4a122b96cfee2f6ca694
Alan Kouakou <alan.kouakou@edu.esiee-it.fr>
2022-09-27 10:35:02 +0200

    Put an item in the list

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from typing import Union
+from pydantic import BaseMode1
 
 app = FastAPI()
 
+class Item(BaseMode1):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 async def root():
@@ -14,6 +19,10 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 @app.get("/items/{item_id}")
-async def read_items(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item-id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name}
 
