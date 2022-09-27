dc3587c0524a6a8b386e4bb0eda44a477cf8074c
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-09-27 14:42:00 +0200

    changing items id function
    Changed our items id function by adding our query parameters

--- main.py
+++ main.py
@@ -22,11 +22,6 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
@@ -51,3 +46,9 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
