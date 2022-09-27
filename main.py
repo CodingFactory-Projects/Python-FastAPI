8d4f23848f96e717450513c32cfab152c89c289e
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-09-27 14:28:04 +0200

    query parameters path

--- main.py
+++ main.py
@@ -43,3 +43,11 @@ async def get_model(model_name: ModelName):
     elif model_name.value == "alan":
         return {"model_name": model_name, "message": "Alan"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
