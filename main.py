76a60e47a3b1b7d4447adcb906ebf2ee0970b1ad
Rui Gaspar <ruigaspar03@hotmail.com>
2022-09-27 11:25:05 +0200

    function to print a name and an age
    function to print a name and an age converted in a string

--- main.py
+++ main.py
@@ -14,17 +14,25 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-def get_full_name(first_name, last_name):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
+# def get_full_name(first_name, last_name):
+#     full_name = first_name.title() + " " + last_name.title()
+#     return full_name
+#
+#
+# print(get_full_name("rui", "gaspar"))
+#
+#
+# def get_full_name(first_name: str, last_name: str):
+#     full_name = first_name.title() + " " + last_name.title()
+#     return full_name
+#
+#
+# print(get_full_name("paul", "walker"))
 
 
-print(get_full_name("rui", "gaspar"))
+def get_name_with_age(name: str, age: int):
+    name_with_age = name.title() + " is this old: " + str(age)
+    return name_with_age
 
 
-def get_full_name(first_name: str, last_name: str):
-    full_name = first_name.title() + " " + last_name.title()
-    return full_name
-
-
-print(get_full_name("paul", "walker"))
+print(get_name_with_age("paul", 49))
