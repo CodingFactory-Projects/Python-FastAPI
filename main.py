3a92e934cd154fe17db9d763a5896bab1a23f28c
Rui Gaspar <ruigaspar03@hotmail.com>
2022-09-27 11:21:22 +0200

    🆕 - functions to print a name
    functions to print a name with a capital letter with the function title()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
@@ -11,3 +12,19 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("rui", "gaspar"))
+
+
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("paul", "walker"))
