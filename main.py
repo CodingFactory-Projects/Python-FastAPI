4e57059fe4c1cf4106deac92782538d06b142f0d
Elim Florvil <elim.florvil@icloud.com>
2022-09-27 15:12:58 +0200

    Update main.py
    requeteavec plusieurs parametres

--- main.py
+++ main.py
@@ -10,20 +10,51 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
-
+## Routes ROOT
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
+## Update all in item by item_id
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_price": item.price, "item_id": item_id}
-@app.get("/people/{name}/{age}")
+## Routes display name,age and short description
+@app.get("/peoples/{name}/{age}")
 def get_name_with_age(name: str, age: int):
     name_with_age = name + " is this old " + str(age)+"years old."
     return {"name": name, "age": age, "description": name_with_age}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+## Routes display item with query parameters (skip,limit)
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+## Read item by item.id with condition for query paramaters
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+## Routes display item with multiple query parameters (skip,limit)
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
