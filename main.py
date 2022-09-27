7e388250bf28daedcddb45ca200dda2efb8d7c97
Alan Kouakou <alan.kouakou@edu.esiee-it.fr>
2022-09-27 16:44:40 +0200

    Post and Delete countries in a list

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
 from fastapi import FastAPI
 from typing import Union
 from enum import Enum
-import pydantic
-print('compiled:', pydantic.compiled)
-#from pydantic import BaseMode1
+from pydantic import BaseModel
+
+# from pydantic import BaseMode1
 
 app = FastAPI()
 
-#class Item(BaseMode1):
-    #name: str
-    #price: float
-    #is_offer: Union[bool, None] = None
+
+# class Item(BaseMode1):
+# name: str
+# price: float
+# is_offer: Union[bool, None] = None
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item-id": item_id, "q": q}
 
-#@app.put("/items/{item_id}")
-#async def update_item(item_id: int, item: Item):
-    #return {"item_id": item_id, "item_name": item.name}
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+# return {"item_id": item_id, "item_name": item.name}
 
 @app.get("/users")
 async def red_users():
     return ["Rick"]
 
+
 class ModelName(str, Enum):
     louis = "louis"
     alan = "alan"
     moha = "moha"
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -47,11 +53,41 @@ async def get_model(model_name: ModelName):
 
     return {"model_name": model_name, "message": "clito"}
 
+
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/names/")
-async def read_name(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+
+@app.get("/names/{item_id}")
+async def read_name(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
 
+class Numbers(BaseModel):
+    id: int
+    name: str
+    price: float
 
+data = {
+    'countries': [
+
+    ]
+}
+#@app.post("/countries/{country_name}")
+#async def create_country(country_name: str):
+    #if country_name in data["countries"]:
+        #return {"data": data, "message": "déjà existant"}
+
+    #else:
+        #data["countries"].append(country_name)
+        #return {"data": data, "message": "a été ajouté"}
+
+#@app.delete("/countries/{country_name}")
+#async def delete_country(country_name: str):
+    #if country_name in data["countries"]:
+        #data["countries"].remove(country_name)
+        #return {"data": data, "message": "a été supprimé"}
+
+    #else:
+        #return {"data": data, "message": "non existant"}
