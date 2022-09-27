8b0f9c7d30b851a40547a76b1cc943f3ca2cda4a
Alan Kouakou <alan.kouakou@edu.esiee-it.fr>
2022-09-27 19:58:16 +0200

    Post and Get methods for Shops
    I created 2 methods, post and get, to create shops and get shops informations.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Shop(BaseModel):
+    country: str
+    city: str
+    adresse: str
+    proprietaire: str
+    loyer: Optional[float] = None
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
+inventory = {}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/get_shop/{shop_id}")
+async def get_shop(shop_id: int = Path(None, description="The ID of the shop you would like to view")):
+    return inventory[shop_id]
+
+@app.post("/create_shop/{shop_id}")
+async def create_shop(shop_id: int, shop: Shop):
+    if shop_id in inventory:
+        return {"Error": "Shop already exists."}
+
+    inventory[shop_id] = shop
+    return inventory[shop_id]
+
