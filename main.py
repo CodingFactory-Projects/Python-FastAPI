21bfab3a4ca0cddb461c6893562d724b67382231
Rui Gaspar <ruigaspar03@hotmail.com>
2022-09-27 11:53:59 +0200

    🆕 - Process items with a name and is price with a dict

--- main.py
+++ main.py
@@ -47,15 +47,25 @@ def get_items(item_a: str, item_b: int, item_c: float, item_d: bool, item_e: byt
 # print(get_items("hello world!", 3481, 452.44, True, b"A"))
 
 
-# def process_items(items: List[str]):
-#     for item in items:
-#         print(item)
-#
-#
+def process_items(items: List[str]):
+    for item in items:
+        print(item)
+
+
 # print(process_items(["hello world!", "hello world2!", "hello world3!"]))
 
-def process_items(items_t: tuple[int, int, str], items_s: set[bytes]):
+
+def process_items2(items_t: tuple[int, int, str], items_s: set[bytes]):
     return items_t, items_s
 
 
-print(process_items((1, 2, "hello world!"), {b"hello", b"world"}))
+# print(process_items2((1, 2, "hello world!"), {b"hello", b"world"}))
+
+
+def process_items3(prices: dict[str, float]):
+    for item_name, item_price in prices.items():
+        print(item_name)
+        print(item_price)
+
+
+print(process_items3({"banana": 4, "apple": 2, "orange": 1.5}))
