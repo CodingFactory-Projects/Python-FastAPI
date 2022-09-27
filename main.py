3482c532d522758967a574d94ef8e7c2464bace4
Rui Gaspar <ruigaspar03@hotmail.com>
2022-09-27 11:27:39 +0200

    🆕 - function with differents Python types

--- main.py
+++ main.py
@@ -35,4 +35,8 @@ def get_name_with_age(name: str, age: int):
     return name_with_age
 
 
-print(get_name_with_age("paul", 49))
+# print(get_name_with_age("paul", 49))
+
+
+def get_items(item_a: str, item_b: int, item_c: float, item_d: bool, item_e: bytes):
+    return item_a, item_b, item_c, item_d, item_d, item_e
