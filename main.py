9c9a032a1c76af718eaa8ce4a87dc1acf599f45b
Elim Florvil <elim.florvil@icloud.com>
2022-09-28 16:29:15 +0200

    refactor

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+import json
 app = FastAPI()
 
 
-
+class Order(BaseModel):
+    products: dict
+    client_id: int
+    total_price: float | None = None
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -19,3 +22,16 @@ async def read_orders():
     with open('data.json','r') as f:
         orders = json.load(f)["orders"]
     return orders
+
+@app.post("/orders/{order_id}")
+async def create_orders(order_id: int, order: Order):
+    result = {
+        "order_id": order_id,
+        "product": order.products,
+        "client_id": order.client_id
+    }
+    newOrder = json.dumbs(result)
+    jsonfile = open("data.json","w")
+    jsonfile.write(newOrder)
+    jsonfile.close()
+    return result
