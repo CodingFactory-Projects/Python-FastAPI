80f95d3564808d3a67d1463dd8a0037aa4e4264d
Elim Florvil <elim.florvil@icloud.com>
2022-09-28 10:58:46 +0200

    Update main.py
    mise en place de la récupération des commandes dans le fichier json

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +13,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/orders")
+async def get_orders():
+    with open('data.json','r') as f:
+        orders = json.load(f)["orders"]
+    return orders
