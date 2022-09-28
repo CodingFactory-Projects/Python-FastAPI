39a0add0b04de2b6367cad7f1779a0fdab6960f0
Elim Florvil <elim.florvil@icloud.com>
2022-09-28 11:02:56 +0200

    Update main.py
    rénommage de la fonction

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -15,7 +16,7 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 @app.get("/orders")
-async def get_orders():
+async def read_orders():
     with open('data.json','r') as f:
         orders = json.load(f)["orders"]
     return orders
