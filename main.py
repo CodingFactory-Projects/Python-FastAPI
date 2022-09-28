2a62c0482fd3c34b9f75142925c189eb083e2b13
Alan Kouakou <alan.kouakou@edu.esiee-it.fr>
2022-09-28 20:35:31 +0200

    Get method from json

--- main.py
+++ main.py
@@ -2,27 +2,52 @@ from fastapi import FastAPI, Path
 from typing import Optional
 from pydantic import BaseModel
 
+import json
+
 app = FastAPI()
 
-class Shop(BaseModel):
+class shops(BaseModel):
+    shop_id: int
     country: str
     city: str
     adresse: str
     proprietaire: str
     loyer: Optional[float] = None
 
+#inventory = {}
 
-inventory = {}
+#@app.get("/get_shops/")
+#async def get_all_shops():
+    #return inventory
 
-@app.get("/get_shop/{shop_id}")
-async def get_shop(shop_id: int = Path(None, description="The ID of the shop you would like to view")):
-    return inventory[shop_id]
+#@app.get("/get_shop/{shop_id}")
+#async def get_shop(shop_id: int = Path(None, description="The ID of the shop you would like to view")):
+    #return inventory[shop_id]
 
-@app.post("/create_shop/{shop_id}")
-async def create_shop(shop_id: int, shop: Shop):
-    if shop_id in inventory:
-        return {"Error": "Shop already exists."}
+@app.get("/shops")
+async def get_shops():
+    with open('data.json') as f:
+        data = json.load(f)
+        return data['shops']
 
-    inventory[shop_id] = shop
-    return inventory[shop_id]
+@app.get("/shops/{shop_id}")
+async def get_shop(shop_id: int):
+    with open('data.json') as f:
+        data = json.load(f)
+    for shop in data['shops']:
+        if shop_id == shop['shop_id']:
+            return shop
+
+
+
+
+
+
+
+
+
+
+
+
+
 
