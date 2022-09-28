7262c122571ccb2729f4d3b8c6df948e1817d456
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-09-29 01:00:51 +0200

    Add function to change user info
    We can now change every value of users ⭕👌

--- models/users.py
+++ models/users.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import json
+
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,6 +15,8 @@ def load_data():
 data = load_data()
 
 
+# Function to get users from data.json
+
 @app.get("/users/")
 async def get_users():
     return data['users']
@@ -25,8 +29,10 @@ async def get_user_by_id(user_id: int):
             return user
 
 
+# Function to post users from data.json
+
 class User(BaseModel):
-    id: int
+    id: int | None = None
     username: str
     money: int | None = None
 
@@ -39,6 +45,8 @@ async def create_user(user: User):
     return 'user'
 
 
+# Function to delete users by id
+
 @app.delete("/users/{user_id}")
 async def delete_users(user_id: int):
     for user in data['users']:
@@ -48,3 +56,29 @@ async def delete_users(user_id: int):
                 f.write(json.dumps(data, indent=2, separators=(',', ': ')))
                 print(data)
             return data
+
+
+# Function to modify 1 element of users info by id
+
+@app.patch("/users/{user_id}")
+async def update_user(user_id: int, user: User):
+    for users in data['users']:
+        if user_id == users['id']:
+            stored_user_data = users
+            stored_user_model = User(**stored_user_data)
+            update_data = user.dict(exclude_unset=True)
+            stored_user_model.copy(update=update_data)
+            with open('data.json', mode="w") as f:
+                f.write(json.dumps(data, indent=2, separators=(',', ': ')))
+                return update_data
+
+
+@app.put("/users/{user_id}")
+async def update_user_all(user_id: int, user: User):
+    for users in data['users']:
+        if users['id'] == user_id:
+            users['username'] = user.username or users['username']
+            users['money'] = user.money or users['money']
+            with open('data.json', mode="w") as f:
+                f.write(json.dumps(data, indent=2, separators=(',', ': ')))
+                return "Sneaker as been updated"
