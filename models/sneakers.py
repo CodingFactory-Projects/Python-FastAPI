eda2caf2accc49aff8fe1be3d354c92233779b81
Rui Gaspar <ruigaspar03@hotmail.com>
2022-09-28 16:48:53 +0200

    ✅ - Get method for sneakers
    Get method with id for sneakers  to get a sneaker or is name

--- models/sneakers.py
+++ models/sneakers.py
@@ -1,26 +1,49 @@
 from fastapi import FastAPI
 import json
 
-with open('data.json') as f:
-    data = json.load(f)
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/data/sneakers")
+class Sneakers(BaseModel):
+    id: int
+    img: str
+    name: str
+    description: str
+    colors: str
+    price: str
+    stock: str
+
+
+@app.get("/data")
+async def get_data():
+    with open('data.json') as f:
+        data = json.load(f)
+        return data
+
+
+@app.get("/sneakers")
 async def get_sneakers():
+    with open('data.json') as f:
+        data = json.load(f)
     return data['sneakers']
 
 
-@app.get("/data/sneakers/{sneaker_id}")
+@app.get("/sneakers/{sneaker_id}")
 async def get_sneaker(sneaker_id: int):
+    with open('data.json') as f:
+        data = json.load(f)
     for sneaker in data['sneakers']:
         if sneaker_id == sneaker['id']:
             return sneaker
 
 
-@app.get("/data/sneakers/name/{sneaker_id}")
+@app.get("/sneakers/name/{sneaker_id}")
 async def get_sneaker_name(sneaker_id: int):
+    with open('data.json') as f:
+        data = json.load(f)
     for sneaker in data['sneakers']:
         if sneaker_id == sneaker['id']:
             return sneaker["name"]
+
