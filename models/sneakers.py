d2a3f790c1a3d0b0d3d8daf2544254325a4b7339
Rui Gaspar <ruigaspar03@hotmail.com>
2022-09-29 19:13:50 +0200

    🪄 - Code refactor and add comments

--- models/sneakers.py
+++ models/sneakers.py
@@ -1,32 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import json
-
-from pydantic import BaseModel
+from classes.SneakersClass import Sneakers, EditedSneaker
 
 app = FastAPI()
 
 
-class Sneakers(BaseModel):
-    id: int
-    img: str
-    name: str
-    description: str
-    colors: str
-    price: str
-    stock: str
-
-
-class EditedSneaker(BaseModel):
-    img: str = None
-    name: str = None
-    description: str = None
-    colors: str = None
-    price: str = None
-    stock: str = None
-
-
 @app.get("/data")
 async def get_data():
+    # get all data from json file
     with open('data.json') as f:
         data = json.load(f)
         return data
@@ -34,6 +15,7 @@ async def get_data():
 
 @app.get("/sneakers")
 async def get_sneakers():
+    # get all sneakers from json file
     with open('data.json') as f:
         data = json.load(f)
     return data['sneakers']
@@ -41,29 +23,38 @@ async def get_sneakers():
 
 @app.get("/sneakers/{sneaker_id}")
 async def get_sneaker(sneaker_id: int):
+    # open the json file and load the data
     with open('data.json') as f:
         data = json.load(f)
+    # loop through the sneakers
     for sneaker in data['sneakers']:
+        # if the sneaker id matches the id in the url return the sneaker info
         if sneaker_id == sneaker['id']:
             return sneaker
+    return HTTPException(status_code=404, detail="Sneaker not found")
 
 
 @app.get("/sneakers/name/{sneaker_id}")
 async def get_sneaker_name(sneaker_id: int):
+    # open the json file and load the data
     with open('data.json') as f:
         data = json.load(f)
+    # loop through the sneakers
     for sneaker in data['sneakers']:
+        # if the sneaker id matches the id in the url return the sneakers name
         if sneaker_id == sneaker['id']:
             return sneaker["name"]
+    return HTTPException(status_code=404, detail="Sneaker not found")
 
 
 # POST METHODS
 
 @app.post("/sneakers")
 async def create_sneaker(sneaker: Sneakers):
-    # ouvrir le fichier json
+    # open the json file and load the data
     with open('data.json') as f:
         data = json.load(f)
+    # with the jso file open in write mode add the new sneaker to the data
     with open('data.json', mode="w") as f:
         data['sneakers'].append(sneaker.dict())
         f.write(json.dumps(data, indent=2, separators=(',', ': ')))
@@ -72,9 +63,12 @@ async def create_sneaker(sneaker: Sneakers):
 
 @app.put("/sneakers/{sneaker_id}")
 async def update_sneaker(sneaker_id: int, edited_sneaker: EditedSneaker):
+    # open the json file and load the data
     with open('data.json') as f:
         data = json.load(f)
+    # loop through the sneakers
     for sneaker in data['sneakers']:
+        # if the sneaker id is the same as the sneaker id in the url
         if sneaker['id'] == sneaker_id:
             sneaker['name'] = edited_sneaker.name or sneaker['name']
             sneaker['img'] = edited_sneaker.img or sneaker['img']
@@ -83,20 +77,24 @@ async def update_sneaker(sneaker_id: int, edited_sneaker: EditedSneaker):
             sneaker['price'] = edited_sneaker.price or sneaker['price']
             sneaker['stock'] = edited_sneaker.stock or sneaker['stock']
             with open('data.json', mode="w") as f:
+                # write the new data to the json file
                 f.write(json.dumps(data, indent=2, separators=(',', ': ')))
                 return "Sneaker as been updated"
-    return "Sneaker not found"
+    return HTTPException(status_code=404, detail="Sneaker not found")
 
 
 @app.delete('/sneaker/{sneaker_id}')
 async def delete_sneaker(sneaker_id: int):
+    # open the json file and load the data
     with open('data.json') as f:
         data = json.load(f)
+    # loop through the sneakers and find the sneaker with the id given
     for sneaker in data['sneakers']:
         if sneaker_id == sneaker['id']:
             data['sneakers'].remove(sneaker)
             with open('data.json', mode="w") as f:
+                # write the new data to the json file
                 f.write(json.dumps(data, indent=2, separators=(',', ': ')))
                 print(data)
             return "Sneaker has been deleted"
-    return "Sneaker not found"
+    return HTTPException(status_code=404, detail="Sneaker not found")
