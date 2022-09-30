08b2ae0ab97aac11668e4d8d5ff21638ed6e458f
Rui Gaspar <ruigaspar03@hotmail.com>
2022-09-30 11:24:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,4 @@ from methodes.orders import *
 from methodes.shops import *
 from methodes.users import *
 
-
 # app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Write in the path /docs to see the documentation"}
